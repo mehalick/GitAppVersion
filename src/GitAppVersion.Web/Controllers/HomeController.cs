fd82335a34d00fede627d8d810e1a122ccab3e08
Andy Mehalick <andy.mehalick@orangejetpack.com>
2017-04-29 12:03:42 +0300

    initial commit

---
+++ src/GitAppVersion.Web/Controllers/HomeController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace GitAppVersion.Web.Controllers
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
